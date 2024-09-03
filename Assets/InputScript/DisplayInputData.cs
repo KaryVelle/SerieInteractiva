164fda4be7a7fb6096d3d394a44e06ae8f77f44f
Kenneth <alcanamay@gmail.com>
2024-09-03 02:15:50 -0600

    CONTROL SERVIBLE

--- Assets/InputScript/DisplayInputData.cs
+++ Assets/InputScript/DisplayInputData.cs
@@ -1,41 +1,55 @@
 using System;
 using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
 
 
 [RequireComponent(typeof(InputData))]
 public class DisplayInputData : MonoBehaviour
 {
-    public Pen _pen;
+    [FormerlySerializedAs("_pen")] public Pen pen;
+    [SerializeField] public StartDissolveAnim dissolveAnim;
+    [SerializeField] private DissolveTextures dissolveTextures;
     public MoveKey moveKey;
     public bool penGrabbed = false;
     private InputData _inputData;
-    private DisplayInputData _displayInputData;
+    [SerializeField] private MoveManager moveManager;
 
-
+    
     private void Start()
     {
-        _displayInputData = GetComponent<DisplayInputData>();
         _inputData = GetComponent<InputData>();
     }
 
     public void Update()
     {
-        if (_inputData._rightController.TryGetFeatureValue(CommonUsages.primaryButton, out var rightPrimBtn))
+        if (!_inputData._rightController.TryGetFeatureValue(CommonUsages.primaryButton, out var rightPrimBtn))return;
         {
-            if (rightPrimBtn && _pen.isGrabbed)
+            if (rightPrimBtn && dissolveAnim.isControllerGrabbed)
             {
-                
-                _pen.Draw();
+                Debug.Log(rightPrimBtn);
+                StartCoroutine(dissolveTextures.DissolveMaterials());
             }
-            else if (_pen.currentDrawing != null)
+            if (rightPrimBtn && pen.isGrabbed)
             {
-                _pen.currentDrawing = null;
+                pen.Draw();
             }
+            else if (pen.currentDrawing != null)
+            {
+                pen.currentDrawing = null;
+            }
+
         }
         
+        if (_inputData._rightController.TryGetFeatureValue(CommonUsages.primary2DAxis, out var right2DAxis))
+        {
+            if (dissolveTextures.dissolveCorEnded)
+            {
+                moveManager.StartMovement();
+            }
+        }
     }
-
    
 }
